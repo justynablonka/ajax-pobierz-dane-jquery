d4000ff470402f5c82ffe1607a89b8f1f149b8fd
justynablonka <justynablonka@users.noreply.github.com>
2021-04-23 14:20:33 +0200

    One method now in comment

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
 
-    $('#pobierz-dane').click(function () {
+     $('#pobierz-dane').click(function () {
 
-        //$.get
-        $.get('https://akademia108.pl/api/ajax/get-post.php')
-            .done(function (data) {
+         //$.get
+         // $.get('https://akademia108.pl/api/ajax/get-post.php')
+             // .done(function (data) {
 
-                let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
-                let pId = $('<p></p>').text(`ID: ${data.id}`);
-                let pTitle = $('<p></p>').text(`Title: ${data.title}`);
-                let pBody = $('<p></p>').text(`Body: ${data.body}`);
-                let hr = $('<hr />');
+                 // let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
+                 // let pId = $('<p></p>').text(`ID: ${data.id}`);
+                 // let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+                 // let pBody = $('<p></p>').text(`Body: ${data.body}`);
+                 // let hr = $('<hr />');
 
-                $('body').append(pUserId);
-                $('body').append(pId);
-                $('body').append(pTitle);
-                $('body').append(pBody);
-                $('body').append(hr);
+                 // $('body').append(pUserId);
+                 // $('body').append(pId);
+                 // $('body').append(pTitle);
+                 // $('body').append(pBody);
+                 // $('body').append(hr);
 
-            })
-            .fail(function (error) {
-                console.error(error);
-            })
+             // })
+             // .fail(function (error) {
+                 // console.error(error);
+             // })
 
 
         //$.getJSON
